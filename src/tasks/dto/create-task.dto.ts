06a60659a0a808891ce0dff8791ee9bc3ad53714
Yevhwnii <mirandoos3223@gmail.com>
2020-09-10 15:05:48 +0300

    validation rules

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -2,7 +2,12 @@
 // to specify shape of the data thru processes.
 // In our case, it is shape of argument accepted by service functions
 
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
