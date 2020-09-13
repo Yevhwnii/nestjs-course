c30e0b4d483c3cdee9d2e7320ece91a1edeb297e
Yevhwnii <mirandoos3223@gmail.com>
2020-09-13 15:58:59 +0300

    outtsourcing task status enum

---
+++ src/tasks/task-status.enum.ts
@@ -0,0 +1,14 @@
+export enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
+// export interface Task {
+//   id: string;
+//   title: string;
+//   description: string;
+//   status: TaskStatus;
+// }
+
+// Is not needed anymore since we have blueprint in entity file
