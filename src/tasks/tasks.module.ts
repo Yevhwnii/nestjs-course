70eeb1edf9d2e31e120979d20fade8f371d115f3
Yevhwnii <mirandoos3223@gmail.com>
2020-09-13 16:00:30 +0300

    allowing DI for task repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
 
 @Module({
+  // Make Task repository be available thru DI in other files
+  // we called .forRoot in app module to import config for all other db connections
+  // and here we use .forFeature in order to include our TaskRepository to the typeorm ecosystem
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
 })
 export class TasksModule {}
