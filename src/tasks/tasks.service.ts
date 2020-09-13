69ac48e0a95d6c6df04c1ed0eaa8842140a82923
Yevhwnii <mirandoos3223@gmail.com>
2020-09-13 16:00:48 +0300

    task service refactoring (data persistance)

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,77 +1,66 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskRepository } from './task.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
 
 // Services are like modules which can be injected in any place and be used
 // They are singletones which means there is only 1 instance of service at the time
 // They contain all the bussiness logic of the application and then injected in constuctor functions and used by other components of an app
 @Injectable()
 export class TasksService {
+  constructor(
+    // Injecting repository
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository,
+  ) {}
+
+  // LEGACY CODE:
   // Making it private to that controllers which use this service cannot directly manipulate the array. They should
   // only be able to manipulate thru method in this service
-  private tasks: Task[] = [];
+  // const tasks: string[] = []
 
-  getAllTasks(): Task[] {
-    return this.tasks;
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
   }
 
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    let tasks = this.getAllTasks();
-
-    if (status) {
-      tasks = tasks.filter(task => task.status === status);
-    }
-
-    if (search) {
-      tasks = tasks.filter(
-        task =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
-
-    return tasks;
-  }
-
-  getTaskById(id: string): Task {
-    const found = this.tasks.find(task => id === task.id);
+  async getTaskById(id: number): Promise<Task> {
+    const found = await this.taskRepository.findOne(id);
 
     // Throws it here, and NestJs on the background will recognize it as Http expection and return response to the client with formatted error
     if (!found) {
       throw new NotFoundException(`Task with provided ID (${id}) not found!`);
     }
-
     return found;
   }
 
-  createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-
-    const task: Task = {
-      id: uuidv4(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-
-    this.tasks.push(task);
-    // Good idea to return created resource to the frontend
-    return task;
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto);
   }
 
-  deleteTaskById(id: string): void {
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter(task => {
-      return task.id !== found.id;
-    });
+  async deleteTask(id: number): Promise<void> {
+    // remove means that you have to firstly retreive the entity from db and then pass entity as an argument for remove method
+    // which is quite expensive, while delete - deletes entity by criteria and does not require any extra work to be done
+    const result = await this.taskRepository.delete(id);
+    // amount of rows affected by query
+    if (result.affected === 0) {
+      throw new NotFoundException(`Could not find requested task (ID: ${id})`);
+    }
   }
 
-  updateStatus(id: string, status: TaskStatus): Task {
-    // task is a reference to the item of an array
-    const task = this.getTaskById(id);
+  async updateStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
     task.status = status;
+    await task.save();
     return task;
   }
+
+  // updateStatus(id: string, status: TaskStatus): Task {
+  //   // task is a reference to the item of an array
+  //   const task = this.getTaskById(id);
+  //   task.status = status;
+  //   return task;
+  // }
 }
