710ec30eab5333bef8181ce1e5ae6d33ebb18ebd
Yevhwnii <mirandoos3223@gmail.com>
2020-09-14 16:25:33 +0300

    import auth module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,12 +3,14 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   // Make Task repository be available thru DI in other files
   // we called .forRoot in app module to import config for all other db connections
   // and here we use .forFeature in order to include our TaskRepository to the typeorm ecosystem
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
+  // When you import some module it means that everyting that this module exports is accesible here also
   controllers: [TasksController],
   providers: [TasksService],
 })
