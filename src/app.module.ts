7c8db79f9dd16524920328a5adaeb87fad7d434b
Yevhwnii <mirandoos3223@gmail.com>
2020-09-14 16:23:54 +0300

    auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     // this is our root module and its configuration will apply to all submodules
     TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
