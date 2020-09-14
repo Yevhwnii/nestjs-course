7176985d73e35bf0c781d1a6b0671365757622bb
Yevhwnii <mirandoos3223@gmail.com>
2020-09-14 16:24:50 +0300

    passport strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+
+// Define strategy on which will check header and validate against secret key
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'topSecret51',
+    });
+  }
+
+  // should be implemented and contains checked and verified payload from constructor
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    return user;
+  }
+}
