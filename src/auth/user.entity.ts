9b173552a91c030c0efd36066e73f9a148e04541
Yevhwnii <mirandoos3223@gmail.com>
2020-09-16 15:28:30 +0300

    One-to-many relationship to the tasks

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -2,10 +2,12 @@ import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { Task } from 'src/tasks/task.entity';
 
 @Entity()
 // Array of unique columns
@@ -22,6 +24,18 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
+
+  // Setting up relaationships between tables
+  // 1 arg: type of the table (entity), in our case Task
+  // 2 arg: since relationship has to be for both sides, inversed column to acess user from task will be task.user
+  // 3 arg: options
+  // eager - when true, whenever we receive user object we can access user.tasks rigth away. Only one side can be eager
+  @OneToMany(
+    type => Task,
+    task => task.user,
+    { eager: true },
+  )
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt);
